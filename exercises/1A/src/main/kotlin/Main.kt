01f2cc4c87aa60e937d0cd09b783e02290bc9b35
danl <danlaudk@yahoo.dk>
2024-03-12 08:14:15 +0100

    either-missing-errortypes

--- exercises/1A/src/main/kotlin/Main.kt
+++ exercises/1A/src/main/kotlin/Main.kt
@@ -4,7 +4,11 @@ import arrow.core.raise.fold
 import dayTen.ParseError
 import dayTen.dayTen
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
 
+@FlowPreview
+@ExperimentalCoroutinesApi
 suspend fun main() {
     dayTen()
 //    fold({ dayTen() }, { println(it) }, { println(it) })
--- exercises/1A/src/main/kotlin/dayTen/DayTen.kt
+++ exercises/1A/src/main/kotlin/dayTen/DayTen.kt
@@ -1,6 +1,7 @@
 package dayTen
 
 import arrow.core.Either
+import arrow.core.Either.*
 import arrow.fx.coroutines.parMap
 import arrow.optics.optics
 import cc.ekblad.konbini.*
@@ -9,20 +10,12 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.fold
 import kotlin.io.path.Path
-import arrow.core.raise.Raise
-import kotlinx.coroutines.flow.map
 
 const val debug = false
 
 sealed class Operations
-data object Noop : Operations()
+object Noop : Operations()
 data class AddX(val value: Int) : Operations()
-
-
-sealed class LogicalError
-data class ParseError(val message: String) : LogicalError()
-data object InvalidInputError : LogicalError()
-
 
 val noopParser = parser {
     string("noop")
@@ -40,18 +33,6 @@ val operationsParser: Parser<Operations> = parser {
     oneOf(noopParser, addXParser)
 }
 
-fun noop(executionState: ExecutionState): ExecutionState {
-    val result = tick(executionState)
-    return pixel(result)
-}
-
-fun addX(executionState: ExecutionState, addX: AddX): ExecutionState {
-    val firstTick = pixel(tick(executionState))
-    val secondTick = pixel(tick(firstTick))
-
-    return ExecutionState.value.modify(secondTick) { it + addX.value }
-}
-
 @optics
 data class ExecutionState(
     val ticks: Int = 0,
@@ -62,48 +43,59 @@ data class ExecutionState(
     companion object
 }
 
-
-fun tick(executionState: ExecutionState): ExecutionState =
-    ExecutionState.ticks.modify(executionState) { it + 1 }
-
-
-fun pixel(executionState: ExecutionState): ExecutionState {
-    // If our current horizontal position overlaps with the register value:
-    // write out '@', else '.'
-    val horizontalPosition = (executionState.ticks - 1) % 40
-    val sprite = listOf(executionState.value - 1, executionState.value, executionState.value + 1)
-    val withBufferUpdate = pixelTheSprite(horizontalPosition, sprite, executionState)
-
-    if (debug) {
-        println("before: $executionState after: $withBufferUpdate")
-    }
-
-    // If we have a finished the line/crtBuffer, write to screen. else nothing
-    return writeLineToScreenBuffer(withBufferUpdate)
-
+fun tick(executionState: ExecutionState): ExecutionState = run {
+    val result = ExecutionState.ticks.modify(executionState) { it + 1 }
+    // Determine if we should append signal strength
+    result
 }
 
-// If our current horizontal position overlaps with the register value, pixelthesprite
-fun pixelTheSprite(horizontalPosition: Int, sprite: List<Int>, executionState: ExecutionState): ExecutionState {
-    return if (horizontalPosition in sprite) {
-        ExecutionState.crtBuffer.modify(executionState) { it + listOf('@') } // wrap in function
+fun <T> List<List<T>>.appendList(l: List<T>): List<List<T>> = this + listOf(l)
+
+fun writeSprite(horizontalPosition: Int, sprite: List<Int>, executionState: ExecutionState): ExecutionState =
+    if (horizontalPosition in sprite) {
+        ExecutionState.crtBuffer.modify(executionState) { it + listOf('#') }
     } else {
-        ExecutionState.crtBuffer.modify(executionState) { it + listOf(' ') }
+        ExecutionState.crtBuffer.modify(executionState) { it + listOf('.') }
     }
-}
 
-fun writeLineToScreenBuffer(executionState: ExecutionState): ExecutionState {
-    return if (executionState.crtBuffer.size == 40) {
+fun writeBufferToScreen(executionState: ExecutionState) =
+    if (executionState.crtBuffer.size == 40) {
         val withScreenWrite = ExecutionState.screen.modify(executionState) { it.appendList(executionState.crtBuffer) }
         ExecutionState.crtBuffer.modify(withScreenWrite) { emptyList() }
     } else {
         executionState
     }
+
+fun pixel(executionState: ExecutionState): ExecutionState = run {
+    // If our current horizontal position overlaps with the register value:
+    // write out '#', else '.'
+    val horizontalPosition = (executionState.ticks - 1) % 40
+    val sprite = listOf(executionState.value - 1, executionState.value, executionState.value + 1)
+    val withBufferUpdate = writeSprite(horizontalPosition, sprite, executionState)
+    if (debug) {
+        println("before: $executionState after: $withBufferUpdate")
+    }
+    // If we have a full crtBuffer, write to screen
+    val withScreenUpdate = writeBufferToScreen(withBufferUpdate)
+    if (debug) {
+        println("$sprite $horizontalPosition $withScreenUpdate")
+    }
+    withScreenUpdate
 }
 
-fun <T> List<List<T>>.appendList(l: List<T>): List<List<T>> = this + listOf(l)
+fun noop(executionState: ExecutionState): ExecutionState = run {
+    val result = tick(executionState)
+    pixel(result)
+}
 
-fun displayScreen(input: List<List<Char>>) {
+
+fun addX(executionState: ExecutionState, addX: AddX): ExecutionState = run {
+    val firstTick = pixel(tick(executionState))
+    val secondTick = pixel(tick(firstTick))
+    ExecutionState.value.modify(secondTick) { it + addX.value }
+}
+
+fun displayScreen(input: List<List<Char>>) = run {
     val columns = 40
     val rows = 6
     (0 until rows).forEach { row ->
@@ -114,28 +106,39 @@ fun displayScreen(input: List<List<Char>>) {
     }
 }
 
-fun parseCustomError(input: String): Either<ParseError, Operations> {
-    return when (val r = operationsParser.parse(input)) {
-        is ParserResult.Ok -> Either.Right(r.result)
-        is ParserResult.Error -> Either.Left(ParseError("Unable to parse. $r "))
-    }
-}
-
-
+@FlowPreview
+@ExperimentalCoroutinesApi
 suspend fun dayTen() {
-    val path = Path("inputFiles/dayTen.txt")
+    val path = Path("inputFiles/dayTenErrors.txt")
     lines(path)
-        .map {l ->
+        .parMap { l ->
             when (val r = operationsParser.parse(l)) {
-                is ParserResult.Ok -> r.result
-                is ParserResult.Error -> throw Error("Unable to parse $l as operation")
+                is ParserResult.Ok -> Either.Right(r.result)
+                is ParserResult.Error -> Either.Left("Unable to parse $l as operation")
+//                is ParserResult.Error -> throw Error("Unable to parse $l as operation")
             }
         }
-        .fold(ExecutionState()) { accumulator, op ->
-            TODO("Not implemented yet")
+        .fold(ExecutionState()) { acc, eitherOp: Either<String, Operations> ->
+            println("op is $eitherOp")
+            when (eitherOp) {
+                is Right<Operations> -> {
+                    when (val op = eitherOp.value) {
+                        is Noop -> noop(acc)
+                        is AddX -> addX(acc, op)
+                    }
+                }
+
+                is Left<String> -> {
+                    println("Error: $eitherOp")
+                    acc
+                }
+//                is Either.Right -> when(val )
+//                Either.
+//                else ->
+            }
         }
-        .also { es ->
-            println(displayScreen(es.screen))
+
+        .also { r ->
+            println(displayScreen(r.screen))
         }
 }
-
